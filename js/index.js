a907cf97271d775d8c9a363246a4a7b8fc083455
Marzieh saadati <85149894+marisaadati@users.noreply.github.com>
2021-09-21 17:37:46 +0430

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-$
+function test1(){
+    if (document.documentElement.scrollTop>100px) {
+        document.querySelector(".screen-scroller").style.backgroundcolor="red";
+    }
+    else{
+        document.querySelector(".screen-scroller").style.backgroundcolor="transparent";
+    }
+}
+window.addEventListener("scroll",test1);
